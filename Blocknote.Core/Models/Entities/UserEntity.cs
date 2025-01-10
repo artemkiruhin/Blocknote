c441032fd7b84df704742ed74f8a5ada97bd8cec
artemkiruhin <temkaunik@gmail.com>
2025-01-10 11:10:43 +0300

    User entity

---
+++ Blocknote.Core/Models/Entities/UserEntity.cs
@@ -0,0 +1,20 @@
+﻿namespace Blocknote.Core.Models.Entities;
+
+public class UserEntity
+{
+    public Guid Id { get; set; }
+    public string Username { get; set; }
+    public string PasswordHash { get; set; }
+    public DateTime RegisteredAt { get; set; }
+
+    public static UserEntity Create(string username, string passwordHash)
+    {
+        return new()
+        {
+            Id = Guid.NewGuid(),
+            Username = username,
+            PasswordHash = passwordHash,
+            RegisteredAt = DateTime.UtcNow
+        };
+    }
+}
