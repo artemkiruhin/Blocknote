9ca54bec6bf22fcdc718168f08ef8d049428c11e
artemkiruhin <temkaunik@gmail.com>
2025-01-10 11:33:01 +0300

    User Service interface

---
+++ Blocknote.Core/Services/Base/IUserService.cs
@@ -0,0 +1,14 @@
+﻿using Blocknote.Core.Models.Dtos;
+
+namespace Blocknote.Core.Services.Base;
+
+public interface IUserService
+{
+    Task<UserDto?> GetInfoAsync(Guid userId);
+    Task<UserDto?> GetByUsernameAsync(string username);
+    Task<UserDto?> GetByUsernameAndPasswordAsync(string username, string password);
+    Task<bool> Register(string username, string passwordHash);
+    Task<bool> ChangePassword(Guid id, string oldPassword, string newPassword);
+    Task<string> Login(string username, string password);
+    Task<bool> DeleteUser(Guid userId);
+}
