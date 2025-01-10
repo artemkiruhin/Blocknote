a0495dd2e375a05450d7490778e268c032dfa345
artemkiruhin <temkaunik@gmail.com>
2025-01-10 23:11:42 +0300

    Repository implementation

---
+++ Blocknote.Core/Database/Repositories/NoteRepository.cs
@@ -0,0 +1,11 @@
+﻿using Blocknote.Core.Database.Repositories.Base;
+using Blocknote.Core.Models.Entities;
+
+namespace Blocknote.Core.Database.Repositories;
+
+public class NoteRepository : BaseCrudRepository<NoteEntity>, INoteRepository
+{
+    public NoteRepository(AppDbContext context) : base(context)
+    {
+    }
+}
---
+++ Blocknote.Core/Database/Repositories/UserRepository.cs
@@ -0,0 +1,11 @@
+﻿using Blocknote.Core.Database.Repositories.Base;
+using Blocknote.Core.Models.Entities;
+
+namespace Blocknote.Core.Database.Repositories;
+
+public class UserRepository : BaseCrudRepository<UserEntity>, IUserRepository
+{
+    public UserRepository(AppDbContext context) : base(context)
+    {
+    }
+}
