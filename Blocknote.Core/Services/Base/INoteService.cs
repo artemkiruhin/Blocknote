479ceee16c8cd81cb785299ac0311780168e928a
artemkiruhin <temkaunik@gmail.com>
2025-01-10 11:36:15 +0300

    Note service interface

---
+++ Blocknote.Core/Services/Base/INoteService.cs
@@ -0,0 +1,12 @@
+﻿using Blocknote.Core.Models.Dtos;
+
+namespace Blocknote.Core.Services.Base;
+
+public interface INoteService
+{
+    Task<NoteDto?> GetInfoAsync(Guid userId, Guid noteId);
+    Task<UserDto?> GetByFullContentAsync(Guid userId, string fullContent);
+    Task<bool> CreateAsync(string title, string? subtitle, string? content, Guid userId);
+    Task<bool> EditAsync(Guid userId, Guid noteId, string title, string? subtitle, string? content);
+    Task<bool> DeleteUser(Guid userId, Guid noteId);
+}
