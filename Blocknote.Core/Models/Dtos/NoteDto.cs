ef67ce36fa6604468d2c612d8a128309318d041c
artemkiruhin <temkaunik@gmail.com>
2025-01-10 11:15:21 +0300

    Note dto

---
+++ Blocknote.Core/Models/Dtos/NoteDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Blocknote.Core.Models.Dtos;
+
+public class NoteDto
+{
+    public string Title { get; set; }
+    public string? Subtitle { get; set; }
+    public string? Content { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime? UpdatedAt { get; set; }
+}
