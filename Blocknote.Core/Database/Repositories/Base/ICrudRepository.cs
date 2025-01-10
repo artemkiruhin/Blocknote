d50be65ccfa08e7b5903e6cdbf5e491eb901a2af
artemkiruhin <temkaunik@gmail.com>
2025-01-10 11:22:34 +0300

    CRUD repo interface

---
+++ Blocknote.Core/Database/Repositories/Base/ICrudRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq.Expressions;
+
+namespace Blocknote.Core.Database.Repositories.Base;
+
+public interface ICrudRepository<TEntity> where TEntity : class
+{
+    Task<TEntity?> GetByIdAsync(Guid id);
+    Task<IEnumerable<TEntity>> GetAllAsync();
+    Task<IEnumerable<TEntity>> FindAsync(Expression<Func<TEntity, bool>> predicate);
+    Task<bool> AddAsync(TEntity entity);
+    Task<bool> EditAsync(TEntity entity);
+    Task<bool> DeleteAsync(Guid id);
+}
