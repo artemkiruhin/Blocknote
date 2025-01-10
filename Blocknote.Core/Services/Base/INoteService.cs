d9bc9aceb5095bd19fff77e937a40c1f3b6e4363
artemkiruhin <temkaunik@gmail.com>
2025-01-10 23:36:52 +0300

    Note Service impl

--- Blocknote.Core/Services/Base/INoteService.cs
+++ Blocknote.Core/Services/Base/INoteService.cs
@@ -5,8 +5,8 @@ namespace Blocknote.Core.Services.Base;
 public interface INoteService
 {
     Task<NoteDto?> GetInfoAsync(Guid userId, Guid noteId);
-    Task<UserDto?> GetByFullContentAsync(Guid userId, string fullContent);
+    Task<IEnumerable<NoteDto>> GetByFullContentAsync(string fullContent);
     Task<bool> CreateAsync(string title, string? subtitle, string? content, Guid userId);
-    Task<bool> EditAsync(Guid userId, Guid noteId, string title, string? subtitle, string? content);
+    Task<bool> EditAsync(Guid userId, Guid noteId, string? title, string? subtitle, string? content);
     Task<bool> DeleteUser(Guid userId, Guid noteId);
 }
---
+++ Blocknote.Core/Services/Entity/NoteService.cs
@@ -0,0 +1,111 @@
+﻿using Blocknote.Core.Database.Repositories.Base;
+using Blocknote.Core.Models.Dtos;
+using Blocknote.Core.Models.Entities;
+using Blocknote.Core.Services.Base;
+
+namespace Blocknote.Core.Services.Entity;
+
+public class NoteService : INoteService
+{
+    private readonly INoteRepository _repository;
+
+    public NoteService(INoteRepository repository)
+    {
+        _repository = repository;
+    }
+
+    public async Task<NoteDto?> GetInfoAsync(Guid userId, Guid noteId)
+    {
+        try
+        {
+            var note = await _repository.GetByIdAsync(noteId);
+            if (note?.UserId != userId) return null;
+            return new NoteDto()
+            {
+                Title = note.Title,
+                Subtitle = note.Subtitle,
+                Content = note.Content,
+                CreatedAt = note.CreatedAt,
+                UpdatedAt = note.UpdatedAt
+            };
+        }
+        catch (Exception e)
+        {
+
+            return null;
+        }
+    }
+
+    public async Task<IEnumerable<NoteDto>> GetByFullContentAsync(string fullContent)
+    {
+        try
+        {
+            var notes = await _repository.FindAsync(x =>
+                string.Concat(x.Content, x.Title, x.Subtitle).Contains(fullContent));
+            var dtos = notes.Select(x => new NoteDto()
+            {
+                Title = x.Title,
+                Subtitle = x.Subtitle,
+                Content = x.Content,
+                CreatedAt = x.CreatedAt,
+                UpdatedAt = x.UpdatedAt
+            });
+            return dtos;
+        }
+        catch (Exception e)
+        {
+            return [];
+        }
+    }
+
+    public async Task<bool> CreateAsync(string title, string? subtitle, string? content, Guid userId)
+    {
+        try
+        {
+            var note = NoteEntity.Create(title, title, subtitle, userId);
+            return await _repository.AddAsync(note);
+        }
+        catch (Exception e)
+        {
+            return false;
+        }
+    }
+
+    public async Task<bool> EditAsync(Guid userId, Guid noteId, string? title, string? subtitle, string? content)
+    {
+        try
+        {
+            var note = await _repository.GetByIdAsync(noteId);
+            if (note == null) return false;
+            
+            if (note.UserId != userId) return false;
+            
+            if (title != null) note.Title = title;
+            if (subtitle != null) note.Subtitle = subtitle;
+            if (content != null) note.Content = content;
+            note.UpdatedAt = DateTime.UtcNow;
+            
+            return await _repository.EditAsync(note);
+        }
+        catch (Exception e)
+        {
+            return false;
+        }
+    }
+
+    public async Task<bool> DeleteUser(Guid userId, Guid noteId)
+    {
+        try
+        {
+            var note = await _repository.GetByIdAsync(noteId);
+            if (note == null) return false;
+            if (note.UserId != userId) return false;
+            
+            return await _repository.DeleteAsync(noteId);
+        }
+        catch (Exception e)
+        {
+            return false;
+        }
+    }
+}
