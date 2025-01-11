87b01097cce113dd9ee57af041f76eff0241ea1d
artemkiruhin <temkaunik@gmail.com>
2025-01-11 11:48:49 +0300

    auth viewmodels created

---
+++ Blocknote.RazorFrontend/Contracts/LoginViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Blocknote.RazorFrontend.Contracts;
+
+public class LoginViewModel
+{
+    [Required(ErrorMessage = "Введите имя пользователя")]
+    public string Username { get; set; } = "";
+    
+    [Required(ErrorMessage = "Введите пароль")]
+    public string Password { get; set; } = "";
+}
---
+++ Blocknote.RazorFrontend/Contracts/RegisterViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Blocknote.RazorFrontend.Contracts;
+
+public class RegisterViewModel
+{
+    [Required(ErrorMessage = "Введите имя пользователя")]
+    public string Username { get; set; } = "";
+    
+    [Required(ErrorMessage = "Введите пароль")]
+    public string Password { get; set; } = "";
+    
+    
+    [Required(ErrorMessage = "Повторите введенный пароль")]
+    public string ConfirmPassword { get; set; } = "";
+}
