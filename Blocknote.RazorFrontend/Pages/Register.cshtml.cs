4de62d14dc9185054128340894208442a04dfdde
artemkiruhin <temkaunik@gmail.com>
2025-01-11 15:30:04 +0300

    Registration logic

--- Blocknote.RazorFrontend/Pages/Register.cshtml.cs
+++ Blocknote.RazorFrontend/Pages/Register.cshtml.cs
@@ -1,4 +1,6 @@
-﻿using Blocknote.RazorFrontend.Contracts;
+﻿using Blocknote.Core.Services.Base;
+using Blocknote.Core.Services.Hasher;
+using Blocknote.RazorFrontend.Contracts;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -6,15 +8,31 @@ namespace Blocknote.RazorFrontend.Pages;
 
 public class Register : PageModel
 {
+    private readonly IUserService _service;
+    private readonly IHashService _hasher;
     [BindProperty] public RegisterViewModel ViewModel { get; set; } = new();
     
+    public Register (IUserService service, IHashService hasher)
+    {
+        _service = service;
+        _hasher = hasher;
+    }
+
     public void OnGet()
     {
         
     }
 
-    public void OnPost()
+    public async Task<IActionResult> OnPost()
     {
-        
+        try
+        {
+            var result = await _service.Register(ViewModel.Username, _hasher.Compute(ViewModel.Password));
+            return result ? RedirectToPage("Login") : RedirectToPage("Register");
+        }
+        catch
+        {
+            return RedirectToPage("Register");
+        }
     }
 }
