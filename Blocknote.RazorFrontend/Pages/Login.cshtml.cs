d929afdb9758b3f892bbb0707eaba7bd9f40ced6
artemkiruhin <temkaunik@gmail.com>
2025-01-11 11:52:27 +0300

    Login vm bind

--- Blocknote.RazorFrontend/Pages/Login.cshtml.cs
+++ Blocknote.RazorFrontend/Pages/Login.cshtml.cs
@@ -1,9 +1,12 @@
-﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using Blocknote.RazorFrontend.Contracts;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Blocknote.RazorFrontend.Pages;
 
 public class Login : PageModel
 {
+    [BindProperty] public LoginViewModel ViewModel { get; set; } = new();
     public void OnGet()
     {
         
