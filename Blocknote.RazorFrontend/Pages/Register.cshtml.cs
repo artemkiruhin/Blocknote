d2e011e63b3944f52fd422057338038b19b960e7
artemkiruhin <temkaunik@gmail.com>
2025-01-11 14:26:06 +0300

    Register page created

---
+++ Blocknote.RazorFrontend/Pages/Register.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Blocknote.RazorFrontend.Contracts;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Blocknote.RazorFrontend.Pages;
+
+public class Register : PageModel
+{
+    [BindProperty] public RegisterViewModel ViewModel { get; set; } = new();
+    
+    public void OnGet()
+    {
+        
+    }
+
+    public void OnPost()
+    {
+        
+    }
+}
