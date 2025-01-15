a25660a6c5f2d31de8de88e1c2ce78d16cd9de77
artemkiruhin <temkaunik@gmail.com>
2025-01-15 22:31:20 +0300

    Note card created

---
+++ frontend/src/components/Notecard.vue
@@ -0,0 +1,23 @@
+<script setup>
+import {defineEmits, ref} from 'vue';
+defineProps({
+  title: String,
+  subtitle: ref(""),
+});
+defineEmits(['open', 'edit', 'delete']);
+</script>
+
+<template>
+  <div class="note-card">
+    <div class="note-card-content">
+      <span class="note-card-title">{{ title }}</span>
+      <span class="note-card-subtitle" v-if="subtitle!==undefined" >{{ subtitle }}</span>
+    </div>
+    <div class="note-card-menu">
+      <div class="menu-item" @click="$emit('open')">Открыть</div>
+      <div class="menu-item" @click="$emit('edit')">Редактировать</div>
+      <div class="menu-item delete" @click="$emit('delete')">Удалить</div>
+    </div>
+  </div>
+</template>
+
