70cf7e9c456dbc7f84c19ba3febb594a57b0a99b
artemkiruhin <temkaunik@gmail.com>
2025-01-15 21:43:08 +0300

    Header created

---
+++ frontend/src/components/Header.vue
@@ -0,0 +1,16 @@
+<script>
+
+</script>
+
+<template>
+  <div class="header">
+    <h1>Мои заметки</h1>
+    <button class="add-note-btn">
+      <svg width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
+        <line x1="12" y1="5" x2="12" y2="19"></line>
+        <line x1="5" y1="12" x2="19" y2="12"></line>
+      </svg>
+      Добавить заметку
+    </button>
+  </div>
+</template>
