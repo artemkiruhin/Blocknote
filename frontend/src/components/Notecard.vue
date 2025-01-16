9a14497bbffed6c03ae9c606d40f6f720eb0a9c6
artemkiruhin <temkaunik@gmail.com>
2025-01-16 22:09:10 +0300

    Modal for note card

--- frontend/src/components/Notecard.vue
+++ frontend/src/components/Notecard.vue
@@ -1,16 +1,21 @@
 <script setup>
-import {defineEmits} from 'vue';
+import {defineEmits, ref} from 'vue';
+import Notemodal from "@/components/Notemodal.vue";
 defineProps({
   id: String,
   title: String,
   subtitle: String,
+  content: String
 });
+
+const showModal = ref(false);
+
 defineEmits(['open', 'edit', 'delete']);
 </script>
 
 <template>
   <div class="note-card">
-    <div class="note-card-content">
+    <div class="note-card-content" @click="showModal = true" >
       <span class="note-card-title">{{ title }}</span>
       <span class="note-card-subtitle" v-if="subtitle" >{{ subtitle }}</span>
     </div>
@@ -20,5 +25,13 @@ defineEmits(['open', 'edit', 'delete']);
       <div class="menu-item delete" @click="$emit('delete')">Удалить</div>
     </div>
   </div>
+  <Notemodal
+      v-if="showModal"
+      :title="title"
+      :subtitle="subtitle"
+      :content="content"
+      :id="id"
+      @close="showModal = false"
+  />
 </template>
 
--- frontend/src/components/Notemodal.vue
+++ frontend/src/components/Notemodal.vue
@@ -2,64 +2,58 @@
 import { ref, watch } from 'vue';
 
 const props = defineProps({
-  note: Object,
+  id: String,
+  title: String,
+  subtitle: String,
+  content: String,
 });
 
 const emit = defineEmits(['save', 'close']);
 
-const id = ref('');
-const title = ref('');
-const subtitle = ref('');
-const content = ref('');
+const localTitle = ref(props.title);
+const localSubtitle = ref(props.subtitle);
+const localContent = ref(props.content);
 
-watch(
-    () => props.note,
-    (newNote) => {
-      if (newNote) {
-        title.value = newNote.title;
-        content.value = newNote.content;
-      } else {
-        title.value = '';
-        content.value = '';
-      }
-    },
-    {immediate: true}
-);
+watch(() => props.title, (newValue) => {
+  localTitle.value = newValue;
+});
+watch(() => props.subtitle, (newValue) => {
+  localSubtitle.value = newValue;
+});
+watch(() => props.content, (newValue) => {
+  localContent.value = newValue;
+});
 
 const handleSave = () => {
-  if (title.value) {
-    emit('save', {
-      title: title.value,
-      subtitle: subtitle.value,
-      content: content.value,
-    });
-    title.value = '';
-    content.value = '';
-  }
+  emit('save', {
+    id: props.id,
+    title: localTitle.value,
+    subtitle: localSubtitle.value,
+    content: localContent.value,
+  });
 };
 </script>
 
 <template>
   <div class="modal">
     <div class="modal-content">
-      <h2 v-if="id" >Редактировать заметку</h2>
-      <h2 v-else>Добавить заметку</h2>
+      <h2>{{ id ? "Редактировать заметку" : "Добавить заметку" }}</h2>
       <input
-          v-model="title"
+          v-model="localTitle"
           class="modal-input"
           placeholder="Заголовок заметки"
           type="text"
       />
       <input
-          v-model="subtitle"
+          v-model="localSubtitle"
           class="modal-input"
-          placeholder="Подзаголовок заметки (необязательно)"
+          placeholder="Подзаголовок заметки"
           type="text"
       />
       <textarea
-          v-model="content"
+          v-model="localContent"
           class="modal-textarea"
-          placeholder="Содержание заметки (необязательно)"
+          placeholder="Содержание заметки"
       ></textarea>
       <div class="modal-buttons">
         <button class="modal-cancel" @click="$emit('close')">Отмена</button>
