32b3d1e96c3a468be22fb3bafe2e8642b7af71a7
artemkiruhin <temkaunik@gmail.com>
2025-01-22 18:58:04 +0300

    Root routing created

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,20 +1,7 @@
 <script setup>
-import Notepage from "@/components/Notepage.vue";
-import {getToken} from '/models/cookie-manager.js'
-import {ref} from "vue";
-
-import Note from "../models/Note.js";
-import AuthPage from "@/components/AuthPage.vue";
-const notes = [
-  new Note("1", "Title 1", "Subtitle 1", "Content 1"),
-  new Note("2", "Title 2", "Subtitle 2", "Content 2"),
-  new Note("3", "Title 3", "Subtitle 3", "Content 3"),
-  new Note("4", "Title 4", "Subtitle 4", "Content 4"),
-  new Note("5", "Title 5", "Subtitle 5", "Content 5")
-];
+import { getToken } from '/models/cookie-manager.js';
 </script>
 
 <template>
-  <AuthPage v-if="!getToken()" />
-  <Notepage v-if="getToken()" :notes="notes || []"/>
+  <router-view />
 </template>
