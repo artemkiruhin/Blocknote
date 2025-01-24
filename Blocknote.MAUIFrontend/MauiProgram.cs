9f5ba86c12886008e1a2b4947bf11f2e4f9aed0c
artemkiruhin <temkaunik@gmail.com>
2025-01-24 21:35:42 +0300

    Routes changed

--- Blocknote.MAUIFrontend/MauiProgram.cs
+++ Blocknote.MAUIFrontend/MauiProgram.cs
@@ -1,4 +1,14 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using System.Text;
+using Blocknote.Core.Database;
+using Blocknote.Core.Database.Repositories;
+using Blocknote.Core.Database.Repositories.Base;
+using Blocknote.Core.Services.Base;
+using Blocknote.Core.Services.Entity;
+using Blocknote.Core.Services.Hasher;
+using Blocknote.Core.Services.Jwt;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 
 namespace Blocknote.MAUIFrontend;
 
@@ -11,8 +21,38 @@ public static class MauiProgram
             .UseMauiApp<App>()
             .ConfigureFonts(fonts => { fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular"); });
 
+        var configuration = builder.Configuration;
+        
         builder.Services.AddMauiBlazorWebView();
 
+        
+        
+        builder.Services.AddDbContext<AppDbContext>(optionsBuilder =>
+        {
+            optionsBuilder.UseNpgsql(configuration.GetConnectionString("Database"));
+            optionsBuilder.UseLazyLoadingProxies();
+        });
+
+        builder.Services.AddScoped<IUserRepository, UserRepository>();
+        builder.Services.AddScoped<INoteRepository, NoteRepository>();
+
+        builder.Services.AddScoped<IUserService, UserService>();
+        builder.Services.AddScoped<INoteService, NoteService>();
+
+        builder.Services.AddScoped<IHashService, Sha256HashService>();
+        builder.Services.AddScoped<IJwtService>(provider =>
+        {
+            var key = Encoding.UTF8.GetBytes(configuration["jwt:key"] ?? throw new NullReferenceException());
+            var issuer = configuration["jwt:issuer"] ?? throw new NullReferenceException();
+            var audience = configuration["jwt:audience"] ?? throw new NullReferenceException();
+            var expires = int.Parse(configuration["jwt:expires"] ?? throw new NullReferenceException());
+
+            return new JwtService(key, issuer, audience, expires);
+        });
+        builder.Services.AddScoped<IHashService, Sha256HashService>();
+        
+        
+        
 #if DEBUG
         builder.Services.AddBlazorWebViewDeveloperTools();
         builder.Logging.AddDebug();
