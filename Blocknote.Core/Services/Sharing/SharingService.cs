d02e88b13681ba77dc02e7f160243e9765eb5471
artemkiruhin <temkaunik@gmail.com>
2025-01-28 11:16:57 +0300

    Sharing service created

---
+++ Blocknote.Core/Services/Sharing/SharingService.cs
@@ -0,0 +1,75 @@
+﻿using Blocknote.Core.Database.Repositories.Base;
+using Blocknote.Core.Models.Dtos;
+using Blocknote.Core.Models.Entities;
+using Blocknote.Core.Services.Hasher;
+
+namespace Blocknote.Core.Services.Sharing;
+
+public class SharingService : ISharingService
+{
+    private readonly IUserRepository _userRepository;
+    private readonly INoteRepository _noteRepository;
+    private readonly ISharingRepository _sharingRepository;
+    private readonly IHashService _hashService;
+
+    public SharingService(IUserRepository userRepository, INoteRepository noteRepository, ISharingRepository sharingRepository, IHashService hashService)
+    {
+        _userRepository = userRepository;
+        _noteRepository = noteRepository;
+        _sharingRepository = sharingRepository;
+        _hashService = hashService;
+    }
+    
+    public async Task<SharingNoteDto> GetSharingCodeAsync(string code, Guid? userId)
+    {
+        var sharingNote = await _sharingRepository.GetByIdAsync(Guid.Parse(code)) ?? throw new KeyNotFoundException();
+        if (DateTime.UtcNow > sharingNote.CloseAt)
+        {
+            await _sharingRepository.DeleteAsync(sharingNote.Id);
+            throw new KeyNotFoundException();
+        }
+        if (sharingNote.Type == nameof(SharingType.Registered) && !userId.HasValue) throw new UnauthorizedAccessException();
+
+        return new SharingNoteDto()
+        {
+            Id = sharingNote.Id,
+            UserId = sharingNote.UserId,
+            CreatedAt = sharingNote.CreatedAt,
+            NoteId = sharingNote.NoteId
+        };
+    }
+    
+    public async Task<IEnumerable<SharingNoteDto>> GetSharingsAsync(Guid userId)
+    {
+        var entities = await _sharingRepository.GetByUserAsync(userId);
+        return entities.Select(x => new SharingNoteDto()
+        {
+            Id = x.Id,
+            UserId = x.UserId,
+            CreatedAt = x.CreatedAt,
+            NoteId = x.NoteId
+        });
+    }
+
+    public async Task<bool> DeleteSharingCodeAsync(string code, Guid userId)
+    {
+        var sharingNote = await _sharingRepository.GetByIdAsync(Guid.Parse(code)) ?? throw new KeyNotFoundException();
+        if (sharingNote.UserId != userId) throw new UnauthorizedAccessException();
+        var result = await _sharingRepository.DeleteAsync(sharingNote.Id);
+        return result;
+    }
+
+    public async Task<string> CreateSharingCodeAsync(Guid userId, Guid noteId, DateTime closeTime, SharingType type = SharingType.All)
+    {
+        var openTime = DateTime.UtcNow;
+        if (closeTime < openTime) throw new ArgumentException("Close time must be greater than open time.");
+        
+        var noteEntity = await _noteRepository.GetByIdAsync(noteId) ?? throw new KeyNotFoundException();
+        if (noteEntity.UserId != userId) throw new UnauthorizedAccessException();
+        
+        var sharingNote = SharingNoteEntity.Create(noteId, userId, closeTime, type);
+        var result = await _sharingRepository.AddAsync(sharingNote);
+        
+        return result ? sharingNote.Id.ToString() : string.Empty;
+    }
+}
