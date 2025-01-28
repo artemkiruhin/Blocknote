f9e89b3c55f6936271df201bf30713aa415a51ca
artemkiruhin <temkaunik@gmail.com>
2025-01-28 09:49:20 +0300

    Sharing note repository created

---
+++ Blocknote.Core/Database/Repositories/SharingNoteRepository.cs
@@ -0,0 +1,17 @@
+﻿using Blocknote.Core.Database.Repositories.Base;
+using Blocknote.Core.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Blocknote.Core.Database.Repositories;
+
+public class SharingNoteRepository : BaseCrudRepository<SharingNoteEntity>, ISharingRepository
+{
+    public SharingNoteRepository(AppDbContext context) : base(context)
+    {
+    }
+
+    public async Task<IEnumerable<SharingNoteEntity>> GetByUserAsync(Guid userId)
+    {
+        return await _dbSet.AsNoTracking().Where(x => x.UserId == userId).ToListAsync();
+    }
+}
