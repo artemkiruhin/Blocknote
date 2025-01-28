fded27192ef0940320ba8703fb12832ccdf68161
artemkiruhin <temkaunik@gmail.com>
2025-01-28 11:16:46 +0300

    Sharing service interface fix

--- Blocknote.Core/Services/Sharing/ISharingService.cs
+++ Blocknote.Core/Services/Sharing/ISharingService.cs
@@ -1,9 +1,11 @@
-﻿namespace Blocknote.Core.Services.Sharing;
+﻿using Blocknote.Core.Models.Dtos;
+
+namespace Blocknote.Core.Services.Sharing;
 
 public interface ISharingService
 {
-    Task<string> GenerateSharingCode(Guid userId, Guid noteId, DateTime closeTime, SharingType type = SharingType.All);
-    Task<string> GetSharingCodeAsync(string code, Guid? userId);
+    Task<SharingNoteDto> GetSharingCodeAsync(string code, Guid? userId);
+    Task<IEnumerable<SharingNoteDto>> GetSharingsAsync(Guid userId);
     Task<bool> DeleteSharingCodeAsync(string code, Guid userId);
-    Task<string> CreateSharingCodeAsync(Guid userId, Guid noteId,  DateTime closeTime, SharingType type = SharingType.All);
+    Task<string> CreateSharingCodeAsync(Guid userId, Guid noteId, DateTime closeTime, SharingType type = SharingType.All);
 }
