f9ee1b424382d78246d3b7862b0cdd60754acb21
artemkiruhin <temkaunik@gmail.com>
2025-01-28 09:41:11 +0300

    Sharing note dto created

---
+++ Blocknote.Core/Models/Dtos/SharingNoteDto.cs
@@ -0,0 +1,15 @@
+﻿namespace Blocknote.Core.Models.Dtos;
+
+public class SharingNoteDto
+{
+    public Guid Id { get; set; }
+    public Guid NoteId { get; set; }
+    public Guid UserId { get; set; }
+    public string Title { get; set; }
+    public string? Subtitle { get; set; }
+    public string? Content { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public string Type { get; set; }
+    public DateTime CloseAt { get; set; }
+    public string AuthorUsername { get; set; }
+}
