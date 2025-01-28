90a7b4353743df4472d3327e2b442f44d9070b35
artemkiruhin <temkaunik@gmail.com>
2025-01-28 09:41:57 +0300

    Sharing service interface GenerateSharingCode mod to async

--- Blocknote.Core/Services/Sharing/ISharingService.cs
+++ Blocknote.Core/Services/Sharing/ISharingService.cs
@@ -2,7 +2,7 @@
 
 public interface ISharingService
 {
-    string GenerateSharingCode(Guid userId, Guid noteId, DateTime closeTime, SharingType type = SharingType.All);
+    Task<string> GenerateSharingCode(Guid userId, Guid noteId, DateTime closeTime, SharingType type = SharingType.All);
     Task<string> GetSharingCodeAsync(string code, Guid? userId);
     Task<bool> DeleteSharingCodeAsync(string code, Guid userId);
     Task<string> CreateSharingCodeAsync(Guid userId, Guid noteId,  DateTime closeTime, SharingType type = SharingType.All);
