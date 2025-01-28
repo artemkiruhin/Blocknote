d0ff659bf88465e0ee2617faa0c473dda3be8705
artemkiruhin <temkaunik@gmail.com>
2025-01-28 09:37:50 +0300

    Sharing entity created

---
+++ Blocknote.Core/Models/Entities/SharingNoteEntity.cs
@@ -0,0 +1,29 @@
+﻿using Blocknote.Core.Services.Sharing;
+
+namespace Blocknote.Core.Models.Entities;
+
+public class SharingNoteEntity
+{
+    public Guid Id { get; set; }
+    public Guid NoteId { get; set; }
+    public Guid UserId { get; set; }
+    public required string Type { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime CloseAt { get; set; }
+    
+    public virtual UserEntity User { get; set; } = null!;
+    public virtual NoteEntity Note { get; set; } = null!;
+    
+    public static SharingNoteEntity Create(Guid noteId, Guid userId, DateTime closeAt, SharingType sharingType = SharingType.All)
+    {
+        return new()
+        {
+            Id = Guid.NewGuid(),
+            NoteId = noteId,
+            UserId = userId,
+            CloseAt = closeAt,
+            CreatedAt = DateTime.UtcNow,
+            Type = nameof(sharingType)
+        };
+    }
+}
