617675c750c1ac1cfdc4ac4042155a66019cb8a1
artemkiruhin <temkaunik@gmail.com>
2025-01-31 12:09:57 +0300

    Enum extension manager created

---
+++ Blocknote.Core/Services/Extensions/EnumExtensionManager.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Reflection;
+
+namespace Blocknote.Core.Services.Extensions
+{
+    public static class EnumExtensionManager
+    {
+        public static string GetDisplayName(this Enum value)
+        {
+            return value.GetType()
+                .GetField(value.ToString())?
+                .GetCustomAttribute<DisplayAttribute>()?
+                .Name ?? value.ToString();
+        }
+
+    }
+}
