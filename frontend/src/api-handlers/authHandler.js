1ac5b75dfdd55bc98a5764d61619a8cb889301e0
artemkiruhin <temkaunik@gmail.com>
2025-03-05 21:03:30 +0300

    add: auth api handler

---
+++ frontend/src/api-handlers/authHandler.js
@@ -0,0 +1,78 @@
+import {API_URL} from "./base-handler";
+
+const login = async (username, password) => {
+    try {
+        const response = await fetch(`${API_URL}/login`, {
+            method: 'POST',
+            body: JSON.stringify({
+                username: username,
+                password: password
+            }),
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            credentials: 'include'
+        })
+
+        if (!response.ok) {
+            console.error(`Ошибка: ${response.statusText} | ${response.status}`)
+        }
+
+        const data = await response.json()
+
+        return data.token
+
+    } catch (e) {
+        console.error("Ошибка авторизации: ", e)
+    }
+}
+
+const register = async (username, password) => {
+    try {
+        const response = await fetch(`${API_URL}/register`, {
+            method: 'POST',
+            body: JSON.stringify({
+                username: username,
+                password: password
+            }),
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            credentials: 'include'
+        })
+
+        if (!response.ok) {
+            console.error(`Ошибка: ${response.statusText} | ${response.status}`)
+        }
+
+        const data = await response.json()
+
+        return data.token
+
+    } catch (e) {
+        console.error("Ошибка регистрации: ", e)
+    }
+}
+const logout = async () => {
+    try {
+        const response = await fetch(`${API_URL}/auth/logout`, {
+            method: 'POST',
+            credentials: 'include'
+        })
+
+        if (!response.ok) {
+            console.error(`Ошибка: ${response.statusText} | ${response.status}`)
+        }
+        return true
+
+    } catch (e) {
+        console.error("Ошибка разлогирования: ", e)
+        return false
+    }
+}
+
+export {
+    login,
+    register,
+    logout,
+}
