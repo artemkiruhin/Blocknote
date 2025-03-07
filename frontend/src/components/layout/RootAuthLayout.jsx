88b55c2b3f9dd2931f11a3c804b1045554290b13
artemkiruhin <temkaunik@gmail.com>
2025-03-07 20:17:36 +0300

    add: auth validate in layout

--- frontend/src/components/layout/RootAuthLayout.jsx
+++ frontend/src/components/layout/RootAuthLayout.jsx
@@ -1,7 +1,34 @@
 import Navbar from "./Navbar";
 import Container from "./Container";
+import {useEffect, useState} from "react";
+import {useNavigate} from "react-router-dom";
+import {validate} from "../../api-handlers/auth-handler";
 
 const RootAuthLayout = ({ children }) => {
+    const navigate = useNavigate();
+    const [isAuthenticated, setIsAuthenticated] = useState(null);
+    const [isLoading, setIsLoading] = useState(true);
+
+    useEffect(() => {
+        const validateAuth = async () => {
+            const result = await validate()
+            setIsAuthenticated(result)
+        }
+
+        validateAuth();
+        setIsLoading(false);
+    }, [])
+
+    if (!isAuthenticated) {
+        navigate("/auth")
+    }
+
+    if (isLoading) {
+        return (
+            <div>Загрузка</div>
+        )
+    }
+
     return (
         <>
             <Navbar />
