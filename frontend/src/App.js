6877d13327a7afd5057841c65478ff2bc897d18e
artemkiruhin <temkaunik@gmail.com>
2025-03-10 21:31:56 +0300

    add: register sharing-by-code-page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,6 +8,7 @@ import Container from "./components/layout/Container";
 import SharingsPage from "./pages/SharingsPage";
 import ShareNotePage from "./pages/SharingNotePage";
 import CreateShareNotePage from "./pages/CreateShareNotePage";
+import SharingByCodePage from "./pages/SharingByCodePage";
 
 const App = () => {
     return (
@@ -19,6 +20,7 @@ const App = () => {
                 <Route path="/sharings" element={<RootAuthLayout><SharingsPage /></RootAuthLayout>} />
                 <Route path="/sharings/:id" element={<RootAuthLayout><ShareNotePage /></RootAuthLayout>} />
                 <Route path="/sharings/new/:id" element={<RootAuthLayout><CreateShareNotePage /></RootAuthLayout>} />
+                <Route path="/sharings/code/:code" element={<SharingByCodePage />} />
                 <Route path="/auth" element={<AuthPage />} />
             </Routes>
         </BrowserRouter>
